4511d12d0fb2a89e4f03d293aedf1af044fc20ce
De2Viator <troshenkoyura@gmail.com>
2023-04-02 14:02:53 +0300

    fix the bug

--- src/app/shopping/components/shopping.component.ts
+++ src/app/shopping/components/shopping.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import {ShoppingService} from "../services/shopping.service";
-import {ActivatedRoute} from "@angular/router";
-import {ShoppedIngredient} from "../models/shopped";
 import {Ingredient} from "../../recipes/models/recipe";
 
 @Component({
@@ -10,10 +8,7 @@ import {Ingredient} from "../../recipes/models/recipe";
   styleUrls: ['../styles/shopping.component.scss']
 })
 export class ShoppingComponent {
-  shoppings:ShoppedIngredient[] = [];
-  constructor(public readonly shoppingService: ShoppingService, private route: ActivatedRoute) {
-    this.shoppings = this.route.snapshot.data['ingredients'] || this.shoppingService.getIngredients();
-  }
+  constructor(public readonly shoppingService: ShoppingService) {}
 
   addIngredient() {
     this.shoppingService.addIngredient(this.shoppingService.uploadedIngredient.value as Ingredient)
